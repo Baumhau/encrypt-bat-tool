28c3f6926612e0b23f035d0f3bcaedce77088630
Baumhau <139624810+Baumhau@users.noreply.github.com>
2024-07-31 18:57:01 +0200

    Add files via upload

---
+++ decrypt.bat
@@ -0,0 +1,32 @@
+@echo off
+cd dateien
+:encryption
+REN *.j9k1 *.pcx
+goto Bild
+:bild 
+REN *8J2n *.png 
+REN *.7k3l *.jpg
+REN *.7a3L *.jpeg
+goto text
+:text
+REN *.g6h1 *.pdf
+REN *.e5g0 *.txt
+REN *.h4t9 *.doc
+REN *.h4rt *.docs
+REN *.z8u0 *.xls
+REN *.z8ü1 *.xml
+goto video
+:video 
+REN  *.m7p6 *.swf
+REN  *.j8q1 *.asf
+REN  *.k0m1 *.avi
+REN  *.l0g5 *.mov
+REN  *.j4l0 *.mp4
+goto audio
+:audio 
+REN *.m2p0 *.mp3
+REN *.k0l1 *.WAV
+REN *.lt7q *.WMA
+exit
+
+
---
+++ encrypt.bat
@@ -0,0 +1,32 @@
+@echo off
+cd dateien
+:encryption
+REN *.pcx *.j9k1
+goto Bild
+:bild 
+REN *.png *.8J2n
+REN *.jpg *.7k3l
+REN *.jpeg *.7a3L
+goto text
+:text
+REN *.pdf *.g6h1
+REN *.txt *.e5g0
+REN *.doc *.h4t9
+REN *.docs *.h4rt
+REN *.xls *.z8u0
+REN *.xml *.z8ü1
+goto video
+:video 
+REN  *.swf *.m7p6
+REN  *.asf *.j8q1
+REN  *.avi *.k0m1
+REN  *.mov *.l0g5
+REN  *mp4  *.j4l0
+goto audio
+:audio 
+REN *.mp3 *.m2p0
+REN *.WAV *.k0l1
+REN *.WMA *.lt7q
+exit
+
+
